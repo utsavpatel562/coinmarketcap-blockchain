36d9da7ed31792e602b3519dea1e88cab8215090
Utsav Patel <158522755+utsavpatel562@users.noreply.github.com>
2025-08-01 09:19:14 -0400

    latest commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+// app/page.tsx or app/page.jsx
 import React from "react";
+import Head from "next/head";
 
-function page() {
+function Page() {
   return (
     <>
+      <Head>
+        <link rel="icon" type="image/x-icon" href="/logo.jpeg" />
+      </Head>
       <div className="bg-slate-200 h-screen p-10">
         <div className="text-2xl text-slate-200 bg-slate-900 p-3 rounded-3xl h-full place-content-center text-center">
           CoinMarketCap - Blockchain (Web 3.0)
@@ -12,4 +17,4 @@ function page() {
   );
 }
 
-export default page;
+export default Page;
